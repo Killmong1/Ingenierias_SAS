4b116b675b71c6ddd392887fc59cc26478000470
Killmong1 <elkin.benitez01@usa.edu.co>
2025-12-13 23:56:14 -0500

    cairo

--- fastapi_back/main.py
+++ fastapi_back/main.py
@@ -108,10 +108,10 @@ app.add_middleware(
     allow_headers=["*"],  # Déjalo solo con "*"
     allow_origins=[
         "https://prueba-tech-rl2t.onrender.com",
-        "http://prueba-tech-rl2t.onrender.com"
+        "http://prueba-tech-rl2t.onrender.com",
         "http://localhost:5173",
         "https://killmong1.github.io/prueba_tech/",
-        "http://killmong1.github.io/prueba_tech/"
+        "http://killmong1.github.io/prueba_tech/",
         "https://killmong1.github.io/prueba_tech",
         "http://killmong1.github.io/prueba_tech"
     ] 
